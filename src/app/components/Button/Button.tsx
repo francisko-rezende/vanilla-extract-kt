8ce5ad39b995393b0639585776ab3fc50a69089c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-05-10 09:25:36 -0300

    chore: create stories for Button

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { buttonStyles } from "./Button.css";
 import { RecipeVariants } from "@vanilla-extract/recipes";
 
-type ButtonProps = React.ComponentProps<"button"> &
+export type ButtonProps = React.ComponentProps<"button"> &
   RecipeVariants<typeof buttonStyles>;
+
 export const Button = ({
   size = "base",
   color = "secondary",
