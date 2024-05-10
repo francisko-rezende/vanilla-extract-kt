56517bfcd9e85600d5f0522415ed102d9ac2e80c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-05-10 07:51:22 -0300

    chore: create color themes and move global theme to new file

--- src/app/components/Button/Button.css.ts
+++ src/app/components/Button/Button.css.ts
@@ -1,27 +1,26 @@
-import { globalTheme } from "@/styles/theme.css";
+import { vars } from "@/styles/theme.css";
 import { style } from "@vanilla-extract/css";
 
 export const buttonStyles = style({
-  borderRadius: globalTheme.borderRadius["rounded-full"],
-  background: globalTheme.colors.primary[500],
-  border: `1px solid ${globalTheme.colors.primary[500]}`,
-  color: globalTheme.colors.white,
-  padding: `${globalTheme.spacing[2]} ${globalTheme.spacing[4]}`,
+  borderRadius: vars.borderRadius["rounded-full"],
+  background: vars.colors.primary[500],
+  border: `1px solid ${vars.colors.primary[500]}`,
+  color: vars.colors.white,
+  padding: `${vars.spacing[2]} ${vars.spacing[4]}`,
   cursor: "pointer",
   transition: "background 200ms",
 
   "@media": {
     "screen and (min-width: 768px)": {
-      borderRadius: globalTheme.borderRadius.rounded,
+      borderRadius: vars.borderRadius.rounded,
     },
   },
-
   ":hover": {
-    background: globalTheme.colors.primary[700],
-    borderColor: globalTheme.colors.primary[700],
+    background: vars.colors.primary[700],
+    borderColor: vars.colors.primary[700],
   },
   ":active": {
-    background: globalTheme.colors.primary[300],
-    borderColor: globalTheme.colors.primary[300],
+    background: vars.colors.primary[300],
+    borderColor: vars.colors.primary[300],
   },
 });
