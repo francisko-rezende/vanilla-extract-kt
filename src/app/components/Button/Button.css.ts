d12ba62fbe694c58143d2399b14d1a739933ad7a
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-05-10 07:26:19 -0300

    chore: implement global theme using createGlobalTheme

--- src/app/components/Button/Button.css.ts
+++ src/app/components/Button/Button.css.ts
@@ -1,14 +1,27 @@
+import { globalTheme } from "@/styles/theme.css";
 import { style } from "@vanilla-extract/css";
 
 export const buttonStyles = style({
-  backgroundColor: "goldenrod",
-  color: "white",
+  borderRadius: globalTheme.borderRadius["rounded-full"],
+  background: globalTheme.colors.primary[500],
+  border: `1px solid ${globalTheme.colors.primary[500]}`,
+  color: globalTheme.colors.white,
+  padding: `${globalTheme.spacing[2]} ${globalTheme.spacing[4]}`,
+  cursor: "pointer",
+  transition: "background 200ms",
+
   "@media": {
-    "screen and (min-width: 500px)": {
-      backgroundColor: "red",
+    "screen and (min-width: 768px)": {
+      borderRadius: globalTheme.borderRadius.rounded,
     },
   },
+
   ":hover": {
-    backgroundColor: "aqua",
+    background: globalTheme.colors.primary[700],
+    borderColor: globalTheme.colors.primary[700],
+  },
+  ":active": {
+    background: globalTheme.colors.primary[300],
+    borderColor: globalTheme.colors.primary[300],
   },
 });
